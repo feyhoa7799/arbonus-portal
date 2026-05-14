08869408d601c75277ca1d5252cf07154c7c1d03
F hakimov <dashkasokolova16@yandex.ru>
2026-05-14 13:37:18 +0300

    Use custom site icon metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,11 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "АртРест Бонус",
   description: "Закрытый корпоративный портал",
+  icons: {
+    icon: "/site-icon.svg",
+    shortcut: "/site-icon.svg",
+    apple: "/site-icon.svg",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
